b974a9960b6ba63bed5ab766a685bf3c4b90ead8
Конарёва Оля <olyakonareva220@gmail.com>
2026-02-04 12:03:05 +0500

    добавлен класс калькулятора

---
+++ Calculator.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace CalculatorApp
+{
+    public static class Calculator
+    {
+        // Сложение двух чисел
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        // Вычитание двух чисел
+        public static double Substract(double a, double b)
+        {
+            return a - b;
+        }
+        // Умножение двух чисел
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        // Деление двух чисел
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
